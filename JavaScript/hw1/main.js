6328de3d530d1de27190c7a5be0bed7bbfcec305
YaroslavKabay <yarko.kabay@gmail.com>
2022-04-14 19:39:01 +0300

    hw4 update

---
+++ JavaScript/hw1/main.js
@@ -0,0 +1,49 @@
+//1)
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let bool1 = 5 < 6;
+let bool2 = 5 === '5';
+
+// console.log(bool1);
+// document.write(a);
+// alert(bool2);
+
+// ^ each variable was checked via console,document and alert.
+
+//2)
+
+// let book = ['homo deus', 500, 'nonfiction',];
+// let book1 = ['homo deus', 500, 'nonfiction', ['Yuval', 'Noah', 'Harari']];
+// console.log(book)
+// console.log(book1)
+
+//3)
+
+let person = FirstName + LastName + MiddleName ;
+let FirstName = 'Yaroslav';
+let LastName = 'Kabay';
+let MiddleName = 'Volodumurovuch';
+console.log(person)
+// console.log(`${FirstName} ${LastName} ${MiddleName}`);
+
+//5)
+
+// let books = [
+//     {'homo deus', 340, 'nonfiction', ['Yuval', 'Noah', 'Haari'] }
+//     {'homo fdeus', 2430, 'drama', ['Yuvl', 'Noah', 'Harri'] }
+//     {'homo odeus', 2300, 'comedy', ['Yval', 'oah', 'Harri'] }
+//
+// ]
+
+
+
+
