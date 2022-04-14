e434e7a46d18a00670163c8fe561518fad363df5
YaroslavKabay <yarko.kabay@gmail.com>
2022-04-14 23:51:51 +0300

    hw4 update

--- JavaScript/hw1/main.js
+++ JavaScript/hw1/main.js
@@ -1,17 +1,17 @@
 //1)
-let a = 'hello';
-let b = 'owu';
-let c = 'com';
-let d = 'ua';
-let e = 1;
-let f = 10;
-let g = -999;
-let h = 123;
-let i = 3.14;
-let j = 2.7;
-let k = 16;
-let bool1 = 5 < 6;
-let bool2 = 5 === '5';
+// let a = 'hello';
+// let b = 'owu';
+// let c = 'com';
+// let d = 'ua';
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let h = 123;
+// let i = 3.14;
+// let j = 2.7;
+// let k = 16;
+// let bool1 = 5 < 6;
+// let bool2 = 5 === '5';
 
 // console.log(bool1);
 // document.write(a);
@@ -27,23 +27,41 @@ let bool2 = 5 === '5';
 // console.log(book1)
 
 //3)
+//
+// let firstName = 'Yaroslav';
+// let lastName = 'Kabay';
+// let middleName = 'Volodumurovuch';
+// // let person = firstName + ' ' + lastName + ' ' + middleName;
+// // or
+// // let person = `${firstName} ${lastName} ${middleName}`
+// console.log(person)
 
-let person = FirstName + LastName + MiddleName ;
-let FirstName = 'Yaroslav';
-let LastName = 'Kabay';
-let MiddleName = 'Volodumurovuch';
-console.log(person)
-// console.log(`${FirstName} ${LastName} ${MiddleName}`);
+//4)
+//
+// let firstname = prompt('enter your first name');
+// let lastname = prompt('enter your last name');
+// let middlename = prompt('enter your middle name');
+// let person = `${firstname} ${lastname} ${middlename}`;
+// console.log (person)
 
-//5)
+// 5)
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c)
+
+// 6)
 
 // let books = [
-//     {'homo deus', 340, 'nonfiction', ['Yuval', 'Noah', 'Haari'] }
-//     {'homo fdeus', 2430, 'drama', ['Yuvl', 'Noah', 'Harri'] }
-//     {'homo odeus', 2300, 'comedy', ['Yval', 'oah', 'Harri'] }
-//
+//     {title: 'homo eus', pages: 340, genre: 'nonfictin', authors: ['Yuval', 'Noah', 'Hri']},
+//     {title: 'hoo deus', pages: 340, genre: 'nonction', authors: ['Yuval', 'Noh', 'Haari']},
+//     {title: 'homo deu', pages: 340, genre: 'nonion', authors: ['Yual', 'oah', 'Haari']},
 // ]
-
-
+// console.log(books [0]);
+// console.log(books [1]);
+// console.log(books [2]);
 
 
