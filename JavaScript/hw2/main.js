54d68be6073d2387a494c1cc9577ae746233fab2
YaroslavKabay <yarko.kabay@gmail.com>
2022-04-19 16:29:04 +0300

    hw1

---
+++ JavaScript/hw2/main.js
@@ -0,0 +1,87 @@
+// - Є змінна х, якій ви надаєте довільне числове значення.
+//     Якщо змінна a не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
+
+// // let digit = 0;
+// // let digit = 1;
+// // let digit = -3;
+//
+// if (digit != 0) {
+//     console.log('true')}
+// else {
+//     console.log('false')
+// }
+
+
+// - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
+// (в першу, другу, третю или четверту частину години).
+
+// let time = 0;
+//
+// if (time >= 0 && time <=15 ) {
+//     console.log('1qtr'); }
+// else if (time >= 16 && time <=30 ) {
+//     console.log('2qtr'); }
+// else if (time >= 31 && time <=45 ) {
+//     console.log('3qtr'); }
+// else if (time >= 46 && time <=59 ) {
+//     console.log('4qtr'); }
+
+
+// - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
+//
+
+// let month = prompt('please paste the needed date') ;
+// if (month >= 1 && month <= 11 ) {
+//     document.write('1decade'); }
+// else if (month >= 12 && month <=22 ) {
+//     document.write('2decade'); }
+// else if (month >= 23 && month <=31) {
+//     document.write('3decade'); }
+
+
+// - Скласти розклад на тиждень за домопоги switch. Користувач вводить порядковий номер дня тижня і на екрані відображається інфа що заплановано на цей день (можна замість плану на день, назву дня англійською).
+//
+
+// switch (prompt('Please choose required day')) {
+//
+//     case '1' :
+//         console.log('monday');
+//         break;
+//     case '2' :
+//         console.log('tuesday');
+//         break;
+//     case '3' :
+//         console.log('wednesday');
+//         break;
+//     case '4' :
+//         console.log('thursday');
+//         break;
+//     case '5' :
+//         console.log('friday');
+//         break;
+//     case '6' :
+//         console.log('saturday');
+//         break;
+//     case '7' :
+//         console.log('sunday');
+//         break;
+// }
+
+
+//     - Користувач вводить або має два числа.
+//         Потрібно знайти та вивести максимальне число з тих двох .
+//         Також потрібно врахувати коли введені рівні числа.
+
+// let a = prompt('1number');
+// let b = prompt('2number')
+//
+// if ( a < b ) {
+//     console.log(b); }
+//
+// else if ( a > b ) {
+//     console.log(a); }
+//
+// else if ( a === b ) {
+//     console.log('the numbers are equal');
+//
+// }
---
+++ JavaScript/test/main.js
@@ -0,0 +1,99 @@
+// 1)// Створити- Створити змінні. Присвоїти кожному з них значення:
+// 
+// let a1 = 'hello';
+// let a2= 'owu';
+// let a3 = 'com';
+// let a4 = 'ua';
+// let a5 = 1;
+// let a6 = 10;
+// let a7 = -999;
+// let a8 = 123;
+// let a9 = 3.14;
+// let a10= 2.7;
+// let a11 = 16;
+// let a12 = true;
+// let a13 = false;
+//
+// console.log(a1)
+// console.log(a2)
+// console.log(a3)
+// console.log(a4)
+// console.log(a5)
+// console.log(a6)
+// console.log(a7)
+// console.log(a8)
+// console.log(a9)
+// console.log(a10)
+// console.log(a11)
+// console.log(a12)
+// console.log(a13)
+
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+
+// 2) // - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+//
+// - Створити об'єкт book з наступними полями :
+// назва, (тип string)
+// кількість сторінок (числовий тип),
+// жанр (string)
+// автори (тип - масив, кожен елемент масиву - це стрінга)
+
+// let book= {
+//     title: 'lord',
+//     pages: 345,
+//     genre: 'comedy',
+//     authors: [ 'lalala', 'aldla', 'lajlasdl']
+//
+// }
+//
+// console.log(book)
+
+
+
+//
+// 3) - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+// //
+
+// let fn = 'kb';
+// let ln = 'ya';
+// let mn = 'vl';
+//
+//
+// let person= `${fn} ${ln} ${mn}`;
+//
+// console.log(person)
+
+
+
+// 4)- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+//
+
+// let a= prompt( 'lalallal');
+// let b= prompt( 'dsafdal');
+// let c= prompt( 'laadfasllal');
+// let person = `${a} ${b} ${c} `
+// console.log(person)
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+console.log()
+//
+// - Створити масив об'єктів з назвою books з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+//
+// © 2022 GitHub, Inc.
+//     Terms
+// Privacy
+// Security
+// Status
+// Docs
+// Contact GitHub
+// Pricing
+// API
+// Training
+// Blog
+// About
