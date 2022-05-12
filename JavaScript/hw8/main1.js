17698cc7e2d3e9c0ce06ee9a8ad574025925af99
YaroslavKabay <yarko.kabay@gmail.com>
2022-05-12 15:35:37 +0300

    hw8

---
+++ JavaScript/hw8/main1.js
@@ -0,0 +1,53 @@
+// - Напишіть код,  котрий :
+
+// -- отримує текст з параграфа з id "content"
+// let contentElement = document.getElementById ('content');
+// console.log(contentElement.innerText);
+
+// -- отримує текст з блоку з id "rules"
+// let rulesElement = document.getElementById ('rules');
+// console.log(rulesElement.innerText);
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+// let contentElement = document.getElementById ('content');
+// contentElement.innerText = ' New text content ';
+// console.log(contentElement.innerText);
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+// let rulesElement = document.getElementById ('rules')
+// rulesElement.innerText = ' New text rules ';
+// console.log(rulesElement.innerText);
+
+// -- змініть кожному елементу колір фону на червоний
+// let contentElement = document.getElementById ('content');
+// let rulesElement = document.getElementById ('rules');
+// let ulElements = document.getElementsByTagName ('ul');
+//
+// rulesElement.style.background= 'red';
+// contentElement.style.background= 'red';
+//
+// for (const ulElement of ulElements) {
+//     ulElement.style.background = 'red';
+// }
+
+// -- змініть кожному елементу колір тексту на синій
+// let contentElement = document.getElementById ('content');
+// let rulesElement = document.getElementById ('rules');
+// let ulElements = document.getElementsByTagName ('ul');
+//
+// rulesElement.style.color = 'blue';
+// contentElement.style.color = 'blue';
+//
+// for (const ulElement of ulElements) {
+//     ulElement.style.color = 'blue';
+// }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+// let rulesElement = document.getElementById ('rules');
+// console.log(rulesElement.classList);
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+// let fc_rulesElements = document.getElementsByClassName ('fc_rules');
+// for (const fc_rulesElement of fc_rulesElements) {
+//     fc_rulesElement.style.color = 'blue';
+// }
---
+++ JavaScript/hw8/main2.js
@@ -0,0 +1,47 @@
+// 1) Напишіть код, який :
+
+// a) додає клас з назвою групи, елементу з ід main_header
+// let idElement = document.getElementById ('main_header');
+// idElement.className = 'Class';
+// console.log(idElement);
+
+// b) робить шириниу елементу ul 400px
+// let ulElements = document.getElementsByTagName ('ul');
+// for (const ulElement of ulElements) {
+//     ulElement.style.width = '400px';
+// }
+
+// c) робить шириниу всіх елементів з класом linkList шириною 50%
+// let Elements = document.getElementsByClassName ('linkList');
+// for (const Element of Elements) {
+//     Element.style.width = '50%';
+//     Element.style.background = 'red';
+// }
+
+// d) отримує текст який зберігається в елементі з класом listElement2
+// let Elements = document.getElementsByClassName ('listElement2');
+// for (const element of Elements) {
+//     console.log(element.innerText);
+// }
+
+// e) отримує всі елементи li та змінює ім колір фону на сірий
+// let liElements = document.getElementsByTagName ('li');
+//
+// for (const liElement of liElements) {
+//     liElement.style.color = 'blue';
+//     console.log(liElement);
+// }
+
+// f) отримує всі елементи 'a' та додає їм клас anchor
+// let tagElement = document.getElementsByTagName('a');
+// tagElement.className = 'anchor';
+// console.log(tagElement);
+
+// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+// h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+// i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+// k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+// l) отримати елементи p та змінити їм padding на 20px
+// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
+//
