e2d4fd1889e958d7a488da045ba682d03771312c
YaroslavKabay <yarko.kabay@gmail.com>
2022-05-13 13:49:48 +0300

    hw8 update

--- JavaScript/hw8/main2.js
+++ JavaScript/hw8/main2.js
@@ -28,7 +28,7 @@
 // let liElements = document.getElementsByTagName ('li');
 //
 // for (const liElement of liElements) {
-//     liElement.style.color = 'blue';
+//     liElement.style.background = 'silver';
 //     console.log(liElement);
 // }
 
@@ -38,10 +38,54 @@
 // console.log(tagElement);
 
 // g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+// let aElements = document.getElementsByTagName ('a');
+// for (const aElement of aElements) {
+//     if (aElement.innerText === 'link3') {
+//         aElement.style.fontSize = '40px';
+//     }
+// }
+
 // h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+// let aElements = document.getElementsByTagName ('a');
+// for (const aElement of aElements) {
+//     aElement.classList.add(`element_${aElement.innerText}`)
+// }
+// console.log(aElements);
+
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
+// let call = prompt('Please choose your background')
+// let Elements = document.getElementsByClassName ('sub-header')
+// for (const element of Elements) {
+//     element.style.background = call
+// }
+
 // j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+// let color = prompt('Please choose your background');
+// let elements = document.getElementsByClassName ('sub-header');
+// for (const element of elements) {
+//     if (element.innerText === 'content 2 segment') {
+//         element.style.color = color;
+//     }
+// }
+
 // k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
-// l) отримати елементи p та змінити їм padding на 20px
-// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
 //
+// let text = prompt('Please enter your text');
+// let elements = document.getElementsByClassName ('content_1');
+// for (const element of elements) {
+//     element.innerText = text;
+// }
+
+// l) отримати елементи p та змінити їм padding на 20px
+// let Elements = document.getElementsByTagName ('p');
+// for (const Element of Elements) {
+//    Element.style.padding ='20px';
+// }
+
+// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
+// let Elements = document.getElementsByClassName ('text2');
+// for (const Element of Elements) {
+//     Element.innerText ='mar-2022';
+// }
+
+
