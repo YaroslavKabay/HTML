c5f0b650e4ae6e201dd8f7ceae91f28fa9cf625b
YaroslavKabay <102923480+YaroslavKabay@users.noreply.github.com>
2022-06-06 11:44:02 +0300

    Update main.js

--- JavaScript/Project/main.js
+++ JavaScript/Project/main.js
@@ -1,14 +1,8 @@
-// В index.html
-// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
-// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
-// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу
-// user-details.html, которая имеет детальную информацию про объект на который кликнули
-
 fetch(`https://jsonplaceholder.typicode.com/users`)
 .then(response => response.json())
 .then(value => {
     let wrap = document.createElement('div');
-    wrap.classList.add('mainDiv');
+    wrap.classList.add('wrap');
             for (const element of value) {
                 let div = document.createElement('div');
                 div.classList.add('div')
@@ -18,7 +12,8 @@ fetch(`https://jsonplaceholder.typicode.com/users`)
 
                 let btn = document.createElement('button')
                 btn.setAttribute('id', 'myButton')
-                btn.innerText = 'Click';
+                btn.classList.add('button')
+                btn.innerText = 'Check';
                 btn.onclick = function () {
                     localStorage.setItem('id', element.id)
                     location.href = 'user-details.html';
