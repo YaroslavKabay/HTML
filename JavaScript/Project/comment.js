d4f7f9a2fb1813ce59a0d3d5fbff85c236c383d7
YaroslavKabay <yaroslav.kabay@gmail.com>
2022-06-18 09:18:54 +0300

    new commit

--- JavaScript/Project/comment.js
+++ JavaScript/Project/comment.js
@@ -4,17 +4,33 @@ let id = localStorage.getItem('id');
 fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(response => response.json())
     .then(value => {
-            console.log(value);
             let div2 = document.createElement('div');
-        div2.classList.add('div2');
-        div2.innerHTML = `
-<h4>Email: ${value.userId}</h4>
-<h4>Email: ${value.id}</h4>
-<h4>Email: ${value.title}</h4>
-<h4>Email: ${value.body}</h4>
+            div2.classList.add('div2');
+            div2.innerHTML = `
+<h3>UserID: ${value.userId}</h3>
+<h4>ID: ${value.id}</h4>
+<h4>Title: ${value.title}</h4>
+<h4>Body: ${value.body}</h4>
 `
-        wrap2.appendChild(div2);
-        document.body.appendChild(wrap2);
-    }
-)
+            wrap2.appendChild(div2);
+            document.body.appendChild(wrap2);
+        }
+    )
 
+fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+    .then(response => response.json())
+    .then(fn => {
+        let postDiv = document.createElement ('div');
+        postDiv.classList.add('postDiv')
+        for (const element of fn) {
+            let commentDiv = document.createElement('div');
+            commentDiv.classList.add('commentDiv')
+            commentDiv.innerHTML = `
+            <h5>Name: ${element.name}</h5>
+            <h5>Email: ${element.email}</h5>
+            <h5>Body: ${element.body}</h5>
+            `
+            postDiv.appendChild(commentDiv)
+            document.body.appendChild(postDiv)
+        }
+    })
--- JavaScript/Project/main.js
+++ JavaScript/Project/main.js
@@ -1,30 +1,25 @@
-// В index.html
-// 1 получить массив объектов user с endpoint`а https://jsonplaceholder.typicode.com/users
-// 2 Вывести id,name всех user в index.html. Отдельный блок для каждого user.
-// 3 Добавить каждому блоку кнопку/ссылку , при клике на которую происходит переход на страницу
-// user-details.html, которая имеет детальную информацию про объект на который кликнули
-
 fetch(`https://jsonplaceholder.typicode.com/users`)
-.then(response => response.json())
-.then(value => {
-    let wrap = document.createElement('div');
-    wrap.classList.add('mainDiv');
-            for (const element of value) {
-                let div = document.createElement('div');
-                div.classList.add('div')
-                div.innerHTML = `
+    .then(response => response.json())
+    .then(value => {
+        let wrap = document.createElement('div');
+        wrap.classList.add('wrap');
+        for (const element of value) {
+            let div = document.createElement('div');
+            div.classList.add('div')
+            div.innerHTML = `
                 <h3>ID: ${element.id}</h3>
                 <h4>Title: ${element.name}</h4>`
 
-                let btn = document.createElement('button')
-                btn.setAttribute('id', 'myButton')
-                btn.innerText = 'Click';
-                btn.onclick = function () {
-                    localStorage.setItem('id', element.id)
-                    location.href = 'user-details.html';
-                };
-                div.appendChild(btn);
-                wrap.appendChild(div);
-                document.body.appendChild(wrap);
-            }
-})
+            let btn = document.createElement('button')
+            btn.setAttribute('id', 'myButton')
+            btn.classList.add('button')
+            btn.innerText = 'Check';
+            btn.onclick = function () {
+                localStorage.setItem('id', element.id)
+                location.href = 'user-details.html';
+            };
+            div.appendChild(btn);
+            wrap.appendChild(div);
+            document.body.appendChild(wrap);
+        }
+    })
